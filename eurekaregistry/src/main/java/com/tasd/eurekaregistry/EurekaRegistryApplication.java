12acd39e2a925d94c6ebf93a9f29d753a54962d1
lorenzodivito <lorenzodivito@mbp-di-lorenzo.homenet.telecomitalia.it>
2018-12-22 17:00:20 +0100

    Adding Eureka Server, configure it and application properties. first configuration of postgree

--- eurekaregistry/src/main/java/com/tasd/eurekaregistry/EurekaRegistryApplication.java
+++ eurekaregistry/src/main/java/com/tasd/eurekaregistry/EurekaRegistryApplication.java
@@ -2,7 +2,8 @@ package com.tasd.eurekaregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaRegistryApplication {
 
