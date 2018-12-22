e9fb6c3072164d52c9b68db0cb4ea876d309ef80
Lorenzo Di Vito <lorenzodivito@MBP-di-Lorenzo.homenet.telecomitalia.it>
2018-12-22 16:38:25 +0100

    Adding Job and Job Service

---
+++ jobcenter/src/main/java/com/tasd/jobcenter/JobCenterApplication.java
@@ -0,0 +1,14 @@
+package com.tasd.jobcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobCenterApplication.class, args);
+	}
+
+}
+
