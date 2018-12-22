a8f7adb6bf003e71c8416aa76f9ea65a6b162413
Lorenzo Di Vito <lorenzodivito@MBP-di-Lorenzo.homenet.telecomitalia.it>
2018-12-22 16:43:40 +0100

    add Eureka Registry

---
+++ eurekaregistry/src/main/java/com/tasd/eurekaregistry/EurekaRegistryApplication.java
@@ -0,0 +1,14 @@
+package com.tasd.eurekaregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaRegistryApplication.class, args);
+	}
+
+}
+
---
+++ eurekaregistry/src/test/java/com/tasd/eurekaregistry/EurekaRegistryApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tasd.eurekaregistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaRegistryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
