42763499ed9022ffd9e835e90fd50f92fc2d6914
Lorenzo Di Vito <lorenzo.divito@hotmail.com>
2019-01-04 17:49:36 +0100

    add job service

---
+++ jobs/src/main/java/com/tasd/jobs/JobsApplication.java
@@ -0,0 +1,14 @@
+package com.tasd.jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsApplication.class, args);
+	}
+
+}
+
---
+++ jobs/src/test/java/com/tasd/jobs/JobsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tasd.jobs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
