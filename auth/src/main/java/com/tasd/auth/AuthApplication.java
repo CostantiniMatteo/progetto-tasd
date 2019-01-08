c9ffca65c3dd1c468573d7217690ea8bf58b088a
AndreaBiaggi <biaggi.jack@gmail.com>
2019-01-08 15:46:57 +0100

    fixed bug ZuulException on first call

--- auth/src/main/java/com/tasd/auth/AuthApplication.java
+++ auth/src/main/java/com/tasd/auth/AuthApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(basePackages = {"com.tasd.auth.proxy"})
 public class AuthApplication {
 
     public static void main(String[] args) {
