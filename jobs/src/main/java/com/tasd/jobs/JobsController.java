d2c35a6774cdf353160dc27e66190b4a5238384e
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2019-01-08 21:56:42 +0100

    Implemented Job service

--- jobs/src/main/java/com/tasd/jobs/JobsController.java
+++ jobs/src/main/java/com/tasd/jobs/JobsController.java
@@ -9,33 +9,70 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class JobsController {
-	
-	@Autowired
-	JobsRepository jobRepository;
-	
-	@RequestMapping(value = "/api/centers/{username}/jobs", method = RequestMethod.POST)
-	public ResponseEntity<JobEntity> newJob(@RequestHeader("X-User-Header") String loggedUser, @PathVariable String username, @RequestBody JobEntity job) throws URISyntaxException {
-		if (!loggedUser.equals(username)){
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-		}
 
-		job.setDateCreation(new Date());
-		job.setUsername(username);
-		JobEntity j = jobRepository.save(job);
-		return ResponseEntity.created(new URI("/api/centers/" + username + "/jobs/" + j.getId())).body(j);
-	}
+    @Autowired
+    JobsRepository jobRepository;
 
-	// PROVA sei una gioia
-	@RequestMapping(value = "/api/centers/{username}/jobs", method = RequestMethod.GET)
-	public ResponseEntity<List<JobEntity>> newJobs(@RequestHeader("X-User-Header") String loggedUser, @PathVariable String username) {
-		if (!loggedUser.equals(username)){
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-		}
+    @RequestMapping(value = "/api/centers/{username}/jobs", method = RequestMethod.POST)
+    public ResponseEntity<JobEntity> newJob(@RequestHeader("X-User-Header") String loggedUser, @PathVariable String username, @RequestBody JobEntity job) throws URISyntaxException {
+        if (!username.equals(loggedUser)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
 
-		return ResponseEntity.ok().body(jobRepository.findAllByUsername(username));
-	}
+        job.setDateCreation(new Date());
+        job.setUsername(username);
+        JobEntity j = jobRepository.save(job);
+        return ResponseEntity.created(new URI("/api/centers/" + username + "/jobs/" + j.getId())).body(j);
+    }
+
+    @RequestMapping(value = "/api/centers/{username}/jobs/", method = RequestMethod.GET)
+    public ResponseEntity<List<JobEntity>> newJobs(@RequestHeader("X-User-Header") String loggedUser, @PathVariable String username) {
+        if (!username.equals(loggedUser)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return ResponseEntity.ok().body(jobRepository.findAllByUsername(username));
+    }
+
+    @RequestMapping(value = "/api/centers/{username}/jobs/{jobId}", method = RequestMethod.GET)
+    public ResponseEntity<JobEntity> newJobs(@RequestHeader("X-User-Header") String loggedUser, @PathVariable String username, @PathVariable long jobId) {
+        if (!username.equals(loggedUser)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        Optional<JobEntity> jobOpt = jobRepository.findById(jobId);
+        if (!jobOpt.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        JobEntity job = jobOpt.get();
+        if (username.equals(job.getUsername())) {
+            return ResponseEntity.ok(job);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @RequestMapping(value = "/api/centers/{username}/jobs/{jobId}", method = RequestMethod.PUT)
+    public ResponseEntity<JobEntity> newJobs(@RequestHeader("X-User-Header") String loggedUser,
+                                             @PathVariable String username,
+                                             @PathVariable long jobId,
+                                             @RequestBody JobEntity job) {
+        if (!username.equals(loggedUser)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        if (job.getId() != jobId || !username.equals(job.getUsername())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        jobRepository.save(job);
+        return ResponseEntity.ok(job);
+    }
+
 
 }
