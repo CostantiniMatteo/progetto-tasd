15691733a5c08ea5ac65806be19539708802f49c
Dario <bahnhof2000@yahoo.it>
2019-01-09 17:56:39 +0100

    login, signup jobcenter, verification token

--- webapp/application.py
+++ webapp/application.py
@@ -1,15 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort, make_response, redirect, url_for
+import requests
+import json
+import jwt
+from base64 import b64decode
+
 app = Flask(__name__, static_url_path='')
 
-@app.route('/')
-def home():
-	user = {'first_name' : "schifo", 'last_name' : "rossi"}
-	return render_template('home.html', user=user)
+BASE_URL = "http://192.168.99.100:8080"
 
 
-@app.route('/login')
+
+@app.route('/dashboard', methods = ['GET'])
+def dashboard():
+    token = request.cookies.get('bearer')
+    j = jwt.decode(token, b64decode("JwtSecretKey"), algorithms=['HS256'])
+    print(j['sub'])
+    return render_template('home.html', user="")
+
+
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
-	return render_template('login.html')
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
+        print(data)
+        r = requests.post(BASE_URL + "/token/generate-token", json=data)
+
+        if r.status_code == 200:
+            token = r.json()['result']['token']
+            print(token)
+
+            resp = make_response(redirect(url_for('dashboard')))
+            resp.set_cookie('bearer', token)
+
+            return resp
+        else:
+            abort(r.status_code)
+
+
+    return render_template('login.html')
+
+
+@app.route('/signup_jobcenter', methods = ['GET', 'POST'])
+def signup_jobcenter():
+
+    if request.method == 'POST':
+        r_json = request.form.to_dict(flat=True)
+        r_json["role"] = "JOB_CENTER"
+        print(r_json)
+        print(requests.post(BASE_URL + "/signup", json=r_json))
+
+    return render_template('signup_jobcenter.html')
 
 
 
