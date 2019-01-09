57e55a527a99b4ec5164f6355569e40b2865e982
AndreaBiaggi <biaggi.jack@gmail.com>
2019-01-09 14:32:30 +0100

    removed folder

--- seekers/src/main/java/com/tasd/seekers/skills/repo/SkillRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tasd.seekers.skills.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tasd.seekers.skills.entities.SkillEntity;
-
-public interface SkillRepository extends JpaRepository<SkillEntity, Long> {
-		
-}
