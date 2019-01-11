02b2268b39d7d666a01b0bc2e2e245c4177cff67
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2019-01-11 15:12:04 +0100

    Fix route suggestions

--- advisor/src/main/java/com/tasd/advisor/AdvisorController.java
+++ advisor/src/main/java/com/tasd/advisor/AdvisorController.java
@@ -7,14 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class AdvisorController {
 
 	@Autowired
 	private JobsRepository jobsRepository;
-	
+
 	@RequestMapping(value = "api/seekers/{user_id}/suggestions", method = RequestMethod.GET)
-	public ResponseEntity<List<JobEntity>> getAllSeekers(@RequestBody String location) {
-		return ResponseEntity.ok().body(jobsRepository.findAllByLocation(location));
+	public ResponseEntity<List<JobEntity>> getAllSeekers() {
+		return ResponseEntity.ok().body(jobsRepository.findAllByLocation("Milan"));
 	}
 }
