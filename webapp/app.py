5215b983e26125490474218cc0e11b266cc0c4df
Getrio <bahnhof2000@yahoo.it>
2019-01-11 23:02:03 +0100

    dashboard and all jobs webapp

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ from base64 import b64decode
 
 app = Flask(__name__, static_url_path='')
 
-BASE_URL = "http://localhost:8080/"
+BASE_URL = "http://localhost:8080"
 
 
 
@@ -18,13 +18,66 @@ def index():
 
 @app.route('/dashboard', methods = ['GET'])
 def dashboard():
-    # j = validate(request)
-    # if j:
-    #     return render_template('dashboard.html', username=j['sub'])
-    # else:
-    #     abort(401)
 
-    return render_template('dashboard.html', name_page="Dashboard")
+    j = validate(request)
+
+    if not j:
+        abort(401)
+
+    user = {'username' : j['sub'], 'role' : j['authorities'][0]}
+
+    header = { "authorization" : "Bearer " + j['token']}
+
+    if user['role'] == 'SEEKER':
+        r_appl = requests.get(
+                BASE_URL + "/api/seekers/" + user['username'] + "/applications/",
+                headers=header
+            )
+
+        appl_id = [a['jobId'] for a in r_appl.json()]
+
+        applications = []
+
+        for a in appl_id:
+            job = requests.get(
+                BASE_URL + "/api/centers/" + user['username'] + "/jobs/" + str(a),
+                headers=header
+            )
+            applications += [job.json()]
+
+        user["applications"] = applications
+
+        r_sugg = requests.get(
+            BASE_URL + "/api/seekers/" + user['username'] + "/suggestions",
+            headers=header
+        )
+
+        user['suggestions'] = r_sugg.json()
+        
+    print(user)
+
+    return render_template('dashboard.html', user=user)
+
+@app.route('/alljobs', methods = ['GET'])
+def all_jobs():
+    j = validate(request)
+
+    if not j:
+        abort(401)
+
+    user = {'username' : j['sub'], 'role' : j['authorities'][0]}
+
+    header = { "authorization" : "Bearer " + j['token']}
+
+    jobs = requests.get(
+        BASE_URL + "/api/centers/" + user['username'] + "/jobs/",
+        headers=header
+    ).json()
+
+    print(jobs)
+
+    return render_template('all_jobs.html', user=user, jobs=jobs)
+
 
 
 @app.route('/login', methods = ['GET', 'POST'])
@@ -76,11 +129,13 @@ def signup_seeker():
 
 
 
+
 def validate(request):
 
     try:
         token = request.cookies.get('bearer')
         j = jwt.decode(token, b64decode("JwtSecretKey"), algorithms=['HS256'])
+        j['token'] = token
     except:
         j = {}
 
@@ -89,3 +144,4 @@ def validate(request):
 
 if __name__ == '__main__':
     app.run(debug=True)
+ù
