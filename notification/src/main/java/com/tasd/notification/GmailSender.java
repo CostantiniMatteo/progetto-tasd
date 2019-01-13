0b1ec0cf89345cf64870ffeec68f2b50fdceb453
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2019-01-13 21:17:26 +0100

    Implementata email con attachment. DA TESTARE

--- notification/src/main/java/com/tasd/notification/GmailSender.java
+++ notification/src/main/java/com/tasd/notification/GmailSender.java
@@ -1,19 +1,22 @@
 package com.tasd.notification;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.security.GeneralSecurityException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.MimetypesFileTypeMap;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.Session;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
 
 import org.springframework.stereotype.Component;
 
@@ -34,13 +37,13 @@ import com.google.api.services.gmail.model.Message;
 
 @Component
 public class GmailSender {
-	private static final String SENDER = "tasd.project@gmail.com";
+    private static final String SENDER = "tasd.project@gmail.com";
     private static final String APPLICATION_NAME = "progetto-tasd";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "/tokens";
     private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_SEND);
     private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-    
+
 //	public void send(String to, String subject, String body)
 //		throws MessagingException {
 //		MimeMessage message = javaMailSender.createMimeMessage();
@@ -53,22 +56,22 @@ public class GmailSender {
 //		helper.setText(body, true);
 //		javaMailSender.send(message);
 //    }
-	
-	public static MimeMessage createEmail(String to, String from, String subject, String bodyText)
-			throws MessagingException {
-		Properties props = new Properties();
-		Session session = Session.getDefaultInstance(props, null);
 
-		MimeMessage email = new MimeMessage(session);
+    public static MimeMessage createEmail(String to, String from, String subject, String bodyText)
+            throws MessagingException {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
 
-		email.setFrom(new InternetAddress(from));
-		email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
-		email.setSubject(subject);
-		email.setText(bodyText);
-		return email;
-	}
+        MimeMessage email = new MimeMessage(session);
 
-	public static Message createMessageWithEmail(MimeMessage emailContent)
+        email.setFrom(new InternetAddress(from));
+        email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
+        email.setSubject(subject);
+        email.setText(bodyText);
+        return email;
+    }
+
+    public static Message createMessageWithEmail(MimeMessage emailContent)
             throws MessagingException, IOException {
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         emailContent.writeTo(buffer);
@@ -79,19 +82,29 @@ public class GmailSender {
         return message;
     }
 
-    public static Message sendMessage(String destination, String subject, String body)
+    public static void sendMessage(String destination, String subject, String body)
             throws MessagingException, IOException, GeneralSecurityException {
-    	final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-    	Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-    	          .setApplicationName(APPLICATION_NAME)
-    	          .build();
-    	Message message = createMessageWithEmail(createEmail(destination, SENDER, subject, body));
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        Message message = createMessageWithEmail(createEmail(destination, SENDER, subject, body));
         message = service.users().messages().send("me", message).execute();
         System.out.println(message + "PORCOOOOOOO");
-        return message;
     }
 
-    
+    public static void sendMessage(String destination, String subject, String body, InputStream in)
+            throws MessagingException, IOException, GeneralSecurityException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        Message message = createMessageWithEmail(getMimeMessage(destination, subject, body, in));
+        service.users().messages().send("me", message).execute();
+        System.out.println(message + "PORCOOOOOOO");
+    }
+
+
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
         InputStream in = GmailSender.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
@@ -107,9 +120,77 @@ public class GmailSender {
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
-	public static void sendMessage(String destination, String subject, String body, InputStream in) {
-		
-	}
-    
+    public static MimeMessage getMimeMessage(String destination, String subject, String body, InputStream in) throws MessagingException {
+        Session s = Session.getDefaultInstance(new Properties(), null);
+        MimeMessage email = new MimeMessage(s);
+
+        email.setFrom(new InternetAddress(SENDER));
+        email.addRecipient(javax.mail.Message.RecipientType.TO,
+                new InternetAddress(destination));
+        email.setSubject(subject);
+
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(body, "text/plain");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+
+        mimeBodyPart = new MimeBodyPart();
+        DataSource source = new InputStreamDataSource(in, "cv");
+
+        mimeBodyPart.setDataHandler(new DataHandler(source));
+        mimeBodyPart.setFileName("cv.pdf");
+
+        multipart.addBodyPart(mimeBodyPart);
+        email.setContent(multipart);
+
+        return email;
+
+    }
+
+
+    public static class InputStreamDataSource implements DataSource {
+
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        private final String name;
+
+        public InputStreamDataSource(InputStream inputStream, String name) {
+            this.name = name;
+            try {
+                int nRead;
+                byte[] data = new byte[16384];
+                while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+                    buffer.write(data, 0, nRead);
+                }
+                inputStream.close();
+                buffer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public String getContentType() {
+            return new MimetypesFileTypeMap().getContentType(name);
+        }
+
+        @Override
+        public InputStream getInputStream() throws IOException {
+            return new ByteArrayInputStream(buffer.toByteArray());
+        }
+
+        @Override
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public OutputStream getOutputStream() throws IOException {
+            throw new IOException("Read-only data");
+        }
+
+    }
+
 
 }
